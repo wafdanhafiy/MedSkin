7d59269ef87350885b320e9b1611eb8868f07f6d
wafdanhafiy <maswafdan@gmail.com>
2022-04-20 23:12:33 +0700

    data penyakit kulit

--- app/src/main/java/com/protel/medskin/utils/dataDummy.kt
+++ app/src/main/java/com/protel/medskin/utils/dataDummy.kt
@@ -20,6 +20,237 @@ object dataDummy {
             "Rajin menjaga kebersihan diri khususnya bagian muka."
         ))
 
+        skins.add(skinsEntity(
+            "Actinic Keratosis",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Alopecia Areata",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Annulare Granuloma",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+// 5
+        skins.add(skinsEntity(
+            "Atopic Dermatitis",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Bullous Pemphigoid",
+            "disebabkan oleh penyakit autoimun yang mana sistem kekebalan tubuh berbalik menyerang jaringan kulit kita.",
+            "pertama berubahnya warna kulit menjadi kemerahan, kehitaman, dan terasa gatal yang umumnya muncul di bagian lipatan kulit dan kemudian setelah beberapa minggu atau bulan, pada permukaan kulit tersebut muncul luka lepuh yang berisi cairan bening bercampur darah.",
+            "hindari mengonsumsi makanan keras atau renyah, hindari aktivitas yang dapat melukai kulit, dan kelola stress dengan baik. jika sudah terkena, dapat menggunakan obat rekomendasi dokter yang dapat membantu meredakan gejala."
+        ))
+
+        skins.add(skinsEntity(
+            "Candidiasis",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Contact Dermatitis",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Contagiosum Molluscum",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+// 10
+        skins.add(skinsEntity(
+            "Dermatitis Seborrheic",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Dermatitis Stasis",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Exanthem Viral",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Genital Warts",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Herpes",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+// 15
+        skins.add(skinsEntity(
+            "Impetigo",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Keratoacanthoma",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Keratosis Seborrheic",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Lichen Planus",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Lichen Sclerosus",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+        // 20
+        skins.add(skinsEntity(
+            "Lupus",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Melanoma",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+        
+        skins.add(skinsEntity(
+            "Morphea",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Onychomycosis",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Phototoxic Reaction",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+// 25
+        skins.add(skinsEntity(
+            "Pityriasis Rosea",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Psoriasis",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Rosacea",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Scabies",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Sclerosis Tuberou",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+// 30
+        skins.add(skinsEntity(
+            "Tinea",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Tumor Vascular",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Urticaria",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
+        skins.add(skinsEntity(
+            "Vasculitis",
+            "dapat disebabkan oleh penyakit autoimun, seperti rheumatoid arthritis, lupus, atau skleroderma. dan juga reaksi alergi terhadap penggunaan obat-obatan tertentu",
+            "umumnya penderita akan merasakan pegal pegal, berkeringat di malam hari, kelelahan, munculnya ruam, demam, hilangnya denyut nadi pada tungkai tubuh, gangguan saraf seperti kebas atau lemas, dan turunnya berat badan",
+            "untuk mencegah penyakit ini terjadi dapat dilakukannya mempertahankan berat badan ideal, mengonsumsi makanan bergizi seimbang seperti sayur dan buah-buahan, rajin berolahraga, mengelola stress dengan baik, dan berhenti merokok. jika sudah terkena dapat meminum obat obatan sesuai anjuran dokter dan jika sudah parah harus dilaksanakan operasi untuk mencegah pecahnya pembuluh darah yang menyempit"
+        ))
+
+        skins.add(skinsEntity(
+            "Vitiligo",
+            "penyebab_placeholder",
+            "gejala_placeholder",
+            "penanggulangan_placeholder"
+        ))
+
         return skins
     }
 
